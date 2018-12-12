33ab68e5a590adaead2bcf93db256483f27c9e19
Anurag Khandelwal <anuragk@berkeley.edu>
2018-12-12 00:01:22 -0800

    Initial commit

---
+++ pbench/handler.py
@@ -0,0 +1,146 @@
+from __future__ import print_function
+
+import os
+import socket
+import sys
+import time
+
+import boto3
+
+import pocket
+
+if sys.version_info[0] < 3:
+    def b(x):
+        return x
+
+
+    def bytes_to_str(x):
+        return x
+else:
+    def b(x):
+        return x.encode('utf-8') if not isinstance(x, bytes) else x
+
+
+    def bytes_to_str(x):
+        return x.decode('utf-8') if isinstance(x, bytes) else x
+
+
+class Logger(object):
+    def __init__(self, f):
+        self.f = f
+
+    def info(self, msg):
+        self._log('INFO', msg)
+
+    def warn(self, msg):
+        self._log('WARN', msg)
+
+    def error(self, msg):
+        self._log('ERROR', msg)
+
+    def _log(self, msg_type, msg):
+        self.f.send(b('{} {}'.format(msg_type, msg).rstrip()))
+
+    def close(self):
+        self.f.send(b('CLOSE'))
+        self.f.shutdown(socket.SHUT_RDWR)
+        self.f.close()
+
+    def abort(self, msg):
+        self.f.send('ABORT:{}'.format(msg))
+        self.f.shutdown(socket.SHUT_RDWR)
+        self.f.close()
+
+
+def _connect_logger(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    return Logger(sock)
+
+
+def _signal(host, port, lambda_id):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    sock.send(b("READY:" + lambda_id))
+    return bytes_to_str(sock.recv(4096))
+
+
+def _copy_results(logger, result):
+    bucket = boto3.resource('s3').Bucket('bench-results')
+    if os.path.isfile(result):
+        logger.info('Copying results @ {} to S3...'.format(result))
+        with open(result, 'rb') as data:
+            bucket.put_object(Key=os.path.join("pocket", os.path.basename(result)), Body=data)
+    else:
+        logger.warn('Result file {} not found'.format(result))
+
+
+def _pocket_write_buffer(p, job_id, lambda_id, num_ops, src, size):
+    with open('pocket_write_%s.txt' % lambda_id) as output:
+        for i in xrange(num_ops):
+            dst_filename = 'tmp-' + lambda_id + '-' + str(i)
+            t0 = time.time()
+            r = pocket.put_buffer(p, src, size, dst_filename, job_id)
+            t1 = time.time()
+            tt = (t1 - t0) * 1e6
+            output.write('%d\n' % tt)
+            if r != 0:
+                raise Exception("put buffer failed: " + dst_filename)
+
+
+def _pocket_read_buffer(p, job_id, lambda_id, num_ops, size):
+    with open('pocket_read_%s.txt' % lambda_id) as output:
+        text_back = " " * size
+        for i in xrange(num_ops):
+            dst_filename = 'tmp-' + lambda_id + '-' + str(i)
+            t0 = time.time()
+            r = pocket.get_buffer(p, dst_filename, text_back, size, job_id)
+            t1 = time.time()
+            tt = (t1 - t0) * 1e6
+            output.write('%d\n' % tt)
+            if r != 0:
+                raise Exception("get buffer failed: " + dst_filename)
+
+
+def lambda_handler(event, context):
+    # Read event info
+    host = event.get('host')
+    log_port = int(event.get('port'))
+    control_port = log_port + 1
+    mode = event.get('mode')
+    lambda_id = event.get('lambda_id')
+    job_id = event.get('batch_id')
+    object_size = event.get('object_size')
+    num_ops = event.get('num_ops')
+    namenode_ip = "10.1.0.10"
+    namenode_port = 9070
+    text = 'a' * object_size
+
+    try:
+        logger = _connect_logger(host, log_port)
+    except Exception as e:
+        print('Exception: {}'.format(e))
+        raise
+
+    logger.info('Event: {}, Context: {}'.format(event, context))
+
+    response = _signal(host, control_port, lambda_id)
+    if response != "RUN":
+        logger.abort("Control returned " + response)
+        return
+
+    # connect to pocket
+    p = pocket.connect(namenode_ip, namenode_port)
+
+    if "write" in mode:
+        _pocket_write_buffer(p, job_id, lambda_id, num_ops, text, object_size)
+
+    if "read" in mode:
+        _pocket_read_buffer(p, job_id, lambda_id, num_ops, object_size)
+
+    pocket.close(p)
+
+    _copy_results(logger, "pocket_write_%s.txt" % lambda_id)
+    _copy_results(logger, "pocket_read_%s.txt" % lambda_id)
+    logger.close()
+    return
---
+++ pbench/lambda_benchmark.py
@@ -0,0 +1,303 @@
+from __future__ import print_function
+
+import argparse
+import datetime
+import errno
+import json
+import multiprocessing
+import os
+import select
+import socket
+import sys
+import time
+from multiprocessing import Process
+
+import boto3
+from botocore.exceptions import ClientError
+from six.moves import configparser
+
+from handler import lambda_handler, bytes_to_str, b
+
+iam_client = boto3.client('iam')
+lambda_client = boto3.client('lambda')
+function_name = 'StorageBenchmark'
+
+NUM_OPS = 2000
+MAX_DATA_SET_SIZE = 2147483648
+
+
+def num_ops(system, value_size):
+    if system == "dynamodb":
+        return min(2 * NUM_OPS, int(MAX_DATA_SET_SIZE / value_size))
+    elif system == "redis" or system == "mmux":
+        return min(50 * NUM_OPS, int(MAX_DATA_SET_SIZE / value_size))
+    return min(NUM_OPS, int(MAX_DATA_SET_SIZE / value_size))
+
+
+def create_function(name):
+    env = dict()
+    lambda_zip = '/tmp/build/lambda.zip'
+    if not os.path.isfile(lambda_zip):
+        code_path = os.path.dirname(os.path.realpath(__file__))
+        num_cpu = multiprocessing.cpu_count()
+        os.system('mkdir -p /tmp/build && cd /tmp/build && cmake {} && make -j {} pkg'.format(code_path, num_cpu))
+    with open(lambda_zip, 'rb') as f:
+        zipped_code = f.read()
+    role = iam_client.get_role(RoleName='aws-lambda-execute')
+    resp = lambda_client.create_function(
+        FunctionName=name,
+        Description='Storage Benchmark',
+        Runtime='python2.7',
+        Role=role['Role']['Arn'],
+        Handler='benchmark_handler.benchmark_handler',
+        Code=dict(ZipFile=zipped_code),
+        Timeout=300,
+        MemorySize=3008,
+        Environment=dict(Variables=env),
+    )
+    print('Created function: {}'.format(resp))
+
+
+def parse_ini(section, conf_file):
+    config = configparser.ConfigParser()
+    config.read(conf_file)
+    return dict(config.items(section))
+
+
+def invoke_lambda(e):
+    lambda_client.invoke(FunctionName=function_name, InvocationType='Event', Payload=json.dumps(e))
+
+
+def invoke_locally(e):
+    f = Process(target=lambda_handler, args=(e, None,))
+    f.start()
+    return f
+
+
+def invoke(args, mode, warm_up, batch_id=str(0), lambda_id=str(0)):
+    e = dict(
+        host=args.host,
+        port=args.port,
+        object_size=args.obj_size,
+        num_ops=args.num_ops,
+        warm_up=warm_up,
+        mode=mode,
+        batch_id=batch_id,
+        lambda_id=lambda_id
+    )
+    if args.invoke:
+        return invoke_lambda(e)
+    elif args.invoke_local:
+        return invoke_locally(e)
+
+
+def invoke_n(args, mode, n, batch_size=1):
+    return [invoke(args, mode, 0, str(i / batch_size), str(i)) for i in range(n)]
+
+
+def is_socket_valid(socket_instance):
+    """ Return True if this socket is connected. """
+    if not socket_instance:
+        return False
+
+    try:
+        socket_instance.getsockname()
+    except socket.error as err:
+        err_type = err.args[0]
+        if err_type == errno.EBADF:
+            return False
+
+    try:
+        socket_instance.getpeername()
+    except socket.error as err:
+        err_type = err.args[0]
+        if err_type in [errno.EBADF, errno.ENOTCONN]:
+            return False
+
+    return True
+
+
+def run_server(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.setblocking(False)
+    s.settimeout(300)
+    try:
+        s.bind((host, port))
+    except socket.error as ex:
+        print('Bind failed: {}'.format(ex))
+        sys.exit()
+    s.listen(5)
+    return s
+
+
+def print_logs(r, msg):
+    for line in msg.splitlines():
+        print('** Function @ {} {} {}'.format(r.getpeername(), datetime.datetime.now(), line))
+
+
+def log_worker(s, num_connections=1, log=True):
+    inputs = [s]
+    outputs = []
+    n_closed = 0
+    while inputs:
+        readable, writable, exceptional = select.select(inputs, outputs, inputs)
+        for r in readable:
+            if r is s:
+                sock, address = r.accept()
+                sock.setblocking(False)
+                inputs.append(sock)
+            else:
+                data = r.recv(4096)
+                msg = bytes_to_str(data.rstrip().lstrip())
+                if 'CLOSE' in msg or not data:
+                    print_logs(r, msg.replace('CLOSE', ''))
+                    inputs.remove(r)
+                    r.close()
+                    n_closed += 1
+                    if n_closed == num_connections:
+                        inputs.remove(s)
+                        s.close()
+                else:
+                    if log:
+                        print_logs(r, msg)
+
+
+def control_worker(s, workers_per_trigger=1, trigger_count=1, trigger_period=0, log=True):
+    inputs = [s]
+    outputs = []
+    ready = []
+    ids = set()
+    run = True
+    while run:
+        readable, writable, exceptional = select.select(inputs, outputs, inputs)
+        for r in readable:
+            if r is s:
+                sock, address = r.accept()
+                sock.setblocking(False)
+                inputs.append(sock)
+            else:
+                data = r.recv(4096)
+                msg = bytes_to_str(data.rstrip().lstrip())
+                if not data:
+                    inputs.remove(r)
+                    r.close()
+                else:
+                    print('DEBUG: [{}]'.format(msg))
+                    i = int(msg.split('READY:')[1])
+                    if log:
+                        print('... Function id={} ready ...'.format(i))
+                    if i not in ids:
+                        if log:
+                            print('... Queuing function id={} ...'.format(i))
+                        ids.add(i)
+                        ready.append((i, r))
+                        if len(ids) == workers_per_trigger * trigger_count:
+                            run = False
+                        else:
+                            print('.. Progress {}/{}'.format(len(ids), workers_per_trigger * trigger_count))
+                    else:
+                        if log:
+                            print('... Aborting function id={} ...'.format(i))
+                        r.send(b('ABORT'))
+                        inputs.remove(r)
+                        r.close()
+
+    print('.. Starting benchmark ..')
+    ready.sort(key=lambda x: x[0])
+    for t in range(trigger_count):
+        for idx in range(t * workers_per_trigger, (t + 1) * workers_per_trigger):
+            i, sock = ready[idx]
+            if log:
+                print('... Running function id={} ...'.format(i))
+            sock.send(b('RUN'))
+        if log:
+            print('.. End of wave ..')
+            print('.. Sleeping for {}s ..'.format(trigger_period))
+        time.sleep(trigger_period)
+    s.close()
+
+
+def log_process(host, port, num_loggers=1, log=True):
+    s = run_server(host, port)
+    if log:
+        print('... Log server listening on {}:{} ...'.format(host, port))
+    p = Process(target=log_worker, args=(s, num_loggers, log))
+    p.start()
+    return p
+
+
+def control_process(host, port, workers_per_trigger=1, trigger_count=1, trigger_period=0, log=True):
+    s = run_server(host, port)
+    if log:
+        print('... Control server listening on {}:{} ...'.format(host, port))
+    p = Process(target=control_worker, args=(s, workers_per_trigger, trigger_count, trigger_period, log))
+    p.start()
+    return p
+
+
+def main():
+    m_help = ('\n\nmode should contain one or more of the following components,\n'
+              'separated by any non-whitespace delimiter:\n'
+              '\tread     - execute read benchmark\n'
+              '\twrite    - execute write benchmark\n'
+              'Examples:\n'
+              '\twrite_read - execute write and read benchmarks, in that order.')
+    parser = argparse.ArgumentParser(description='Run pocket benchmark on AWS Lambda.',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--create', action='store_true', help='create AWS Lambda function')
+    parser.add_argument('--invoke', action='store_true', help='invoke AWS Lambda function')
+    parser.add_argument('--invoke-local', action='store_true', help='invoke AWS Lambda function locally')
+    parser.add_argument('--quiet', action='store_true', help='Suppress function logs')
+    parser.add_argument('--quieter', action='store_true', help='Suppress all logs')
+    parser.add_argument('--host', type=str, default=socket.gethostname(), help='name of host where script is run')
+    parser.add_argument('--port', type=int, default=8888, help='port that server listens on')
+    parser.add_argument('--num-ops', type=int, default=-1, help='number of operations')
+    parser.add_argument('--obj-size', type=int, default=8, help='object size to benchmark for')
+    parser.add_argument('--mode', type=str, default='write_read', help='benchmark mode' + m_help)
+    args = parser.parse_args()
+
+    if args.create:
+        try:
+            create_function(function_name)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceConflictException':
+                print('Skipping function creation since it already exists...')
+            else:
+                print('Unexpected error: {}'.format(e))
+                raise
+        print('Creation successful!')
+
+    if args.invoke or args.invoke_local:
+        log_function = not args.quiet and not args.quieter
+        log_control = not args.quieter
+        host = args.host
+        log_port = args.port
+        control_port = log_port + 1
+        if args.num_ops == -1:
+            args.num_ops = num_ops(args.system, args.obj_size)
+        if args.mode.startswith('scale'):
+            _, mode, workers_per_trigger, trigger_period, num_triggers = args.mode.split(':')
+            workers_per_trigger = int(workers_per_trigger)
+            trigger_period = int(trigger_period)
+            num_triggers = int(num_triggers)
+            num_functions = workers_per_trigger * num_triggers
+            print('.. Number of functions to launch = {} ..'.format(num_functions))
+            lp = log_process(host, log_port, num_functions, log_function)
+            op = control_process(host, control_port, workers_per_trigger, num_triggers, trigger_period, log_control)
+            processes = invoke_n(args, mode, num_functions)
+            processes.append(lp)
+            processes.append(op)
+        else:
+            lp = log_process(host, log_port)
+            op = control_process(host, control_port)
+            processes = [invoke(args, args.mode, 1), lp, op]
+
+        for p in processes:
+            if p is not None:
+                p.join()
+                print('... {} terminated ...'.format(p))
+
+
+if __name__ == '__main__':
+    main()
